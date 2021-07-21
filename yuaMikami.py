4843a1bb5cf78fc0ae474972b08e5c492040c2eb
SaintLaurentxTran <82715789+SaintLaurentxTran@users.noreply.github.com>
2021-07-21 23:42:33 +0700

    Update yuaMikami.py

--- yuaMikami.py
+++ yuaMikami.py
@@ -1,6 +1,4 @@
 # Cinebench R15 Đây là công cụ đánh giá hiệu năng của card đồ họa sử dụng OpenGL.
 # Bài test quan trọng là sử dụng khung cảnh 3 chiếc xe hơi 3D tại một con đường ánh sáng phức tạp để thử nghiệm khả năng của card đồ họa.
 # Khi đó card phải xử lý gần 1 triệu đa giác với nhiều hiệu ứng đặc biệt được kích hoạt.
-# Kết quả cuối cùng là tốc độ khung hình khi xử lý các khung cảnh đó, đơn vị làfps.
-# Tất nhiên, fps càng cao thì scene mà bạn thấy được sẽ càng mượt mà.
-# Đây là phần mềm hoàn toàn miễn phí, giao diện khá thân thiện và dễ dàng thao tác.
+# Yua Mikami is my idol <3
