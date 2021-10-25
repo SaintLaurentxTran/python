21f5f1023b4827f654919162e541678511837c4e
SaintLaurentxTran <thanhmoicotoi@gmail.com>
2021-10-25 16:52:01 +0700

    up up

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,12 @@
-print('Hello, World!')
-# hello xin chào các bạn
+print('Nhập năm sinh của mày vào đây')
+yearOfBirth = int(input(': '))
+age = 2021 - yearOfBirth
+
+if age > 16:
+    print("máy bay")
+elif age < 16:
+    print("not máy bay")
+else:
+    print("khít")
+
+# Code by Thanhmoiz
