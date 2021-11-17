53779eae81d7d2d12f839ff57d8538211957f464
SaintLaurentxTran <thanhmoicotoi@gmail.com>
2021-11-17 17:51:06 +0700

    update

--- helloworld.py
+++ helloworld.py
@@ -1,14 +1,16 @@
-print('Nhập năm sinh của mày vào đây')
-yearOfBirth = int(input(': '))
-age = 2021 - yearOfBirth
+# print('Nhập năm sinh của mày vào đây')
+# yearOfBirth = int(input(': '))
+# age = 2021 - yearOfBirth
 
-if age > 16:
-    print("máy bay")
-elif age < 16:
-    print("không phải là máy bay")
-else:
-    print("khít")
+# if age > 16:
+#     print("máy bay")
+# elif age < 16:
+#     print("không phải là máy bay")
+# else:
+#     print("khít")
 
 # Code by Thanhmoizdeptraigalangnhatvutru
+
+
 
 
